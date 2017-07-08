da92feca0cb4e888f5669e2446672ec0902e9723
Quentin JEROME <qjerome@users.noreply.github.com>
2017-07-08 16:22:05 +0200

    Requirements file added

---
+++ requirements.go
@@ -0,0 +1,10 @@
+evtx/...
+package main
+
+import (
+	_ "github.com/0xrawsec/golang-utils/datastructs"
+	_ "github.com/0xrawsec/golang-utils/encoding"
+	_ "github.com/0xrawsec/golang-utils/log"
+)
+
+func main(){}
